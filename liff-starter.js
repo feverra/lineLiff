817cca9c8fb01f1db32bfdbdf9aed1f68304e425
unknown <feverra@gmail.com>
2019-10-20 03:50:53 +0700

    a

--- liff-starter.js
+++ liff-starter.js
@@ -7,23 +7,23 @@ window.onload = function () {
 
     // if node is used, fetch the environment variable and pass it to the LIFF method
     // otherwise, pass defaultLiffId
-    if (useNodeJS) {
-        fetch('/send-id')
-            .then(function (reqResponse) {
-                return reqResponse.json();
-            })
-            .then(function (jsonResponse) {
-                myLiffId = jsonResponse.id;
-                initializeLiffOrDie(myLiffId);
-            })
-            .catch(function (error) {
-                document.getElementById("liffAppContent").classList.add('hidden');
-                document.getElementById("nodeLiffIdErrorMessage").classList.remove('hidden');
-            });
-    } else {
+    // if (useNodeJS) {
+    //     fetch('/send-id')
+    //         .then(function (reqResponse) {
+    //             return reqResponse.json();
+    //         })
+    //         .then(function (jsonResponse) {
+    //             myLiffId = jsonResponse.id;
+    //             initializeLiffOrDie(myLiffId);
+    //         })
+    //         .catch(function (error) {
+    //             document.getElementById("liffAppContent").classList.add('hidden');
+    //             document.getElementById("nodeLiffIdErrorMessage").classList.remove('hidden');
+    //         });
+    // } else {
         myLiffId = defaultLiffId;
         initializeLiffOrDie(myLiffId);
-    }
+    // }
 };
 
 /**
